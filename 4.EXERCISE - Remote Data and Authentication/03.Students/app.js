b2dada1995ae2b10fed141730f80312ea35f4d41
Radostina Georgieva <145570050+RadostGeorgieva@users.noreply.github.com>
2024-03-06 21:27:34 +0200

    fixed bug with load event listener and data posting

--- 4.EXERCISE - Remote Data and Authentication/03.Students/app.js
+++ 4.EXERCISE - Remote Data and Authentication/03.Students/app.js
@@ -1,5 +1,6 @@
 function solve() {
-    document.addEventListener('submit', onSubmit)
+    window.addEventListener('load',showData);
+    document.querySelector('form').addEventListener('submit', onSubmit)
     let url = 'http://localhost:3030/jsonstore/collections/students/';
 
     async function onSubmit(event) {
@@ -8,31 +9,30 @@ function solve() {
         const formData = new FormData(event.target);
         const data = Object.fromEntries(formData.entries());
         addData(data);
-        const clearFields = (arr) => arr.forEach(x => x.value = '');
-        clearFields([...document.querySelectorAll('#form > div.inputs > input[type=text]')])
-
+        
     }
-
+    
     async function addData(data) {
-        if ((data.firstName == "" && typeof(data.firstName) == "string") || (data.lastName == "" && typeof(data.lastName) == "string")||(data.facultyNumber == ""&& typeof(data.facultyNumber) == "string") || (data.grade == ""&& typeof(data.grade) == "number")) {
+        if (data.firstName == "" ||data.lastName == ""||data.facultyNumber == ""|| data.grade == "") {
             console.log("all fields are mandatory");
             return;
         }
         try {
             const response = await fetch(url, {
                 method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({ firstName: data.firstName, lastName: data.lastName, facultyNumber: data.facultyNumber, grade: Number(data.grade) })
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(data)
             }
             )
             showData()
         } catch (err) {
+            console.log(data);
             throw new Error(err.message);
         }
     }
     async function showData() {
+        const clearFields = (arr) => arr.forEach(x => x.value = '');
+        clearFields([...document.querySelectorAll('#form > div.inputs > input[type=text]')])
         let url = 'http://localhost:3030/jsonstore/collections/students/';
             const response = await fetch(url);
             const data = await response.json();
